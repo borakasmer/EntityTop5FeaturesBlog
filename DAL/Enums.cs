fb6e9f949e38bc8bce62f5154ac1b9121a4102ef
Bora KAŞMER <bora@borakasmer.com>
2024-11-16 18:45:16 +0300

    Add files via upload

---
+++ DAL/Enums.cs
@@ -0,0 +1,14 @@
+namespace DAL.Entities
+{
+    public enum ProductCategoryEnum : byte
+    {
+        Beverages = 1,
+        Condiments,
+        Confections,
+        DairyProducts,
+        GrainsCereals,
+        MeatPoultry,
+        Produce,
+        Seafood
+    }
+}
